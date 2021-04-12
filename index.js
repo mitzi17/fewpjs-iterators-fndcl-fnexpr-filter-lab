d2164dadeb8e25275e3d31cc2e15a7cba7c8b08d
Mitzi17 <m_estrada17@live.com>
2021-04-12 11:20:51 -0700

    commit tree 619d1e627ef8ef7b91f7cff3e5ec4d8a4f8d5f1f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+let findMatching = (array, string) => {
+    return array.filter(el => el.toLowerCase() === string.toLowerCase())
+}
+
+let fuzzyMatch = (array, string) => {
+    return array.filter(el => el.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+  
+let matchName = (array, string) => {
+    return array.filter(el => el.name === string)
+}
+
